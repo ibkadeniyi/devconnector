a6153a1b65a0ad401111347e8e18332049fe5c58
Ibukun Adeniyi <ibkadeniyi@gmail.com>
2020-08-27 02:38:07 +0100

    completed user auth api for login and registration

---
+++ models/Profile.js
@@ -0,0 +1,16 @@
+/** @format */
+
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+});
