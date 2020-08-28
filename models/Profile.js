1b4460234e85905f5c490cce1ca89244636bbd90
Ibukun Adeniyi <ibkadeniyi@gmail.com>
2020-08-28 08:32:57 +0100

    created profile endpoints

--- models/Profile.js
+++ models/Profile.js
@@ -13,4 +13,100 @@ const ProfileSchema = new mongoose.Schema({
   website: {
     type: String,
   },
+  location: {
+    type: String,
+  },
+  status: {
+    type: String,
+    require: true,
+  },
+  skills: {
+    type: [String],
+    require: true,
+  },
+  bio: {
+    type: String,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        require: true,
+      },
+      company: {
+        type: String,
+        require: true,
+      },
+      location: {
+        type: String,
+      },
+      from: {
+        type: Date,
+        require: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        require: true,
+      },
+      degree: {
+        type: String,
+        require: true,
+      },
+      fieldofstudy: {
+        type: String,
+        require: true,
+      },
+      from: {
+        type: Date,
+        require: true,
+      },
+      to: {
+        type: Date,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
+    },
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
